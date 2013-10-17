77e38c6e977ac907ed31be1c901a8e4b50126b6e
dendek <kasper.minciel@gmail.com>
2013-10-18 01:23:07 +0200

    Now commands are working (finally)!

--- __init__.py
+++ __init__.py
@@ -1,97 +1,12 @@
-#import sys
-#import argparse
-#
-#
-#from .commands.build import Build
-#from .commands.deploy import Deploy
-#from .commands.view import View
-#from .commands.run import Run
-#
-#
-#class UserInterface:
-#
-#    def __init__(self):
-#
-#        # Available commands.
-#
-#        self.commands = {
-#            Build.name: Build(),
-#            Deploy.name: Deploy(),
-#            View.name: View(),
-#            Run.name: Run()
-#        }
-#
-#        # Create command line parser.
-#
-#        self.parser = argparse.ArgumentParser()
-#        subparsers = self.parser.add_subparsers()
-#
-#        # Add subparsers from commands.
-#
-#        for i in self.commands.values():
-#             i.install(subparsers.add_parser(i.name, add_help=False))
-#
-#
-#    def __call__(self, arguments=None):
-#        """Run stado with given arguments"""
-#
-#        # Show help message if no arguments.
-#
-#        #if len(sys.argv) == 1:
-#        #    #self.commands['help'].run()
-#        #    sys.exit(0)                         # 0 = successful termination
-#
-#        # Arguments from sys.args or from method arguments.
-#        if not arguments:
-#            args = self.parser.parse_args()
-#        else:
-#            args = self.parser.parse_args(arguments.split())
-#
-#        # Execute command.
-#        args = vars(args)
-#        print(args)
-#        if 'cmd' in args:
-#
-#
-#            cmd = args.pop('cmd')
-#
-#            if cmd(**args):
-#                sys.exit(0)         # Success.
-#            else:
-#                sys.exit(1)         # Fail.
-#        return False
+import sys
+from commands import CommandLineInterface
+
+console = CommandLineInterface()
 
 
-    ## Continue code execution.
-    #
-    #def stop_watching(self):
-    #    """Stops watching for changes in sites."""
-    #    self.generator.monitor.stop()
-    #
-    #def stop_serving(self):
-    #    """Stops server."""
-    #    server = self.generator.server
-    #    if server:
-    #        server.stop()
-    #
-    ## Events.
-    #
-    #def event_after_building(self):
-    #    """Runs after build command."""
-    #    pass
-    #
-    #def event_after_rebuilding(self):
-    #    """Runs after rebuilding triggered by site monitor."""
-    #    pass
+if __name__ == "__main__":
 
-#import sys
-#from commands import UserInterface
-#
-#
-#framework = UserInterface()
-#
-#if __name__ == "__main__":
-#    if framework.__call__():
-#        sys.exit(0)
-#    else:
-#        sys.exit(1)
+    if console():
+        sys.exit(0)
+    else:
+        sys.exit(1)
