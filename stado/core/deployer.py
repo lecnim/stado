fd6ca997c7a7e435fa8a83cc70b5632695d02c6c
dendek <kasper.minciel@gmail.com>
2013-10-19 04:09:56 +0200

    Fixed making directory which already exists.

--- stado/core/deployer.py
+++ stado/core/deployer.py
@@ -14,13 +14,15 @@ class Deployer:
         """Writes content to path."""
 
         full_path = os.path.join(self.path, path)
+        dir_path = os.path.split(full_path)[0]
 
         # Use custom output path instead of default.
         if CONFIG.output:
             full_path = os.path.join(CONFIG.output, path)
 
         # Create missing directories.
-        os.makedirs(os.path.split(full_path)[0], exist_ok=True)
+        if not os.path.exists(dir_path):
+            os.makedirs(dir_path, exist_ok=True)
 
         with open(full_path, mode='w', encoding='utf-8') as file:
             file.write(content)
