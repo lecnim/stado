17ec0c29478a469c3fc9c29b25bfebf3a5ba7a5b
dendek <kasper.minciel@gmail.com>
2013-10-20 00:00:34 +0200

    Fixed executing compile.py.

--- runtests.py
+++ runtests.py
@@ -4,14 +4,14 @@ import subprocess
 
 
 # Testing source.
-print('Testing stado source...')
+print('Testing stado source...\n')
 nose.run()
 
 # Compiling.
 print('')
-subprocess.call('py -3 compile.py', shell=True)
+subprocess.call('python compile.py', shell=True)
 
 # Testing compiled package.
-print('Testing stado compiled package...')
+print('Testing stado compiled package...\n')
 sys.path.insert(0, 'build/stado.py')
 nose.run()
