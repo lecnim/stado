4569e940e210f94be018dd68d450d0b8dc3d08d3
dendek <kasper.minciel@gmail.com>
2013-10-21 18:25:01 +0200

    Changed Content.permalink property.

--- stado/core/content.py
+++ stado/core/content.py
@@ -15,26 +15,33 @@ class Content:
 
     def __init__(self, source):
 
-        # TODO: Remove this property.
-        self.path = source
         # Path to source file relative to site source, for example: 'a/b.html'
         self.source = source
         # Title of source file, for example: 'b.html'
         self.filename = os.path.split(self.source)[1]
 
         # Content will be available under this URL.
-        self.permalink = '/:path/:filename'
+        self._permalink = '/:path/:filename'
 
         # Template engine renders page using this variables.
         self.template = ''
         self.context = {}
 
     @property
-    def destination(self):
+    def permalink(self):
+        return '/' + self.output
+
+    @permalink.setter
+    def permalink(self, value):
+        self._permalink = value
+
+
+    @property
+    def output(self):
         """Permalink converted to file system path."""
 
-        keywords = re.findall("(:[a-zA-z]*)", self.permalink)
-        destination = os.path.normpath(self.permalink)
+        keywords = re.findall("(:[a-zA-z]*)", self._permalink)
+        destination = os.path.normpath(self._permalink)
 
         items = {
             'path': os.path.split(self.source)[0],
@@ -48,11 +55,12 @@ class Content:
             if key[1:] in items:
                 destination = destination.replace(key, str(items[key[1:]]))
 
+        # //home/a.html => home/a.html
         return destination.lstrip(os.sep)
 
 
     def __repr__(self):
-        return 'Content ' + self.source
+        return "<Content:  '{}'>".format(self.source)
 
 
 
