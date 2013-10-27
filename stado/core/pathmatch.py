8a46db9ae5d0a19389a390d9d7784e590bfdbc2c
lecnim <kasper.minciel@gmail.com>
2013-10-27 21:04:28 +0100

    Added pathmatch module, new path matching system.

---
+++ stado/core/pathmatch.py
@@ -0,0 +1,56 @@
+import re
+
+def translate(pat):
+    """Translate a shell PATTERN to a regular expression.
+
+    There is no way to quote meta-characters.
+    """
+
+    i, n = 0, len(pat)
+    res = ''
+    while i < n:
+
+
+        if pat[i:i+2] == '**':
+            i = i + 2
+
+            res = res + '.*'
+
+            if len(pat) == i:
+                break
+
+        c = pat[i]
+        i = i + 1
+
+        if c == '*':
+            res = res + '[^\/]+'
+
+        elif c == '?':
+            res = res + '.'
+        elif c == '[':
+            j = i
+            if j < n and pat[j] == '!':
+                j = j+1
+            if j < n and pat[j] == ']':
+                j = j+1
+            while j < n and pat[j] != ']':
+                j = j+1
+            if j >= n:
+                res = res + '\\['
+            else:
+                stuff = pat[i:j].replace('\\','\\\\')
+                i = j+1
+                if stuff[0] == '!':
+                    stuff = '^' + stuff[1:]
+                elif stuff[0] == '^':
+                    stuff = '\\' + stuff
+                res = '%s[%s]' % (res, stuff)
+        else:
+            res = res + re.escape(c)
+    return res + '\Z(?ms)'
+
+
+def pathmatch(path, pattern):
+
+    if re.match(translate(pattern), path):
+        return True
