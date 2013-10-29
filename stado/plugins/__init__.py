542a5f0b9ae0daa47ac7c7b135ec19c06092e022
lecnim <kasper.minciel@gmail.com>
2013-10-29 23:37:45 +0100

    Added comments to Extension class.

--- stado/plugins/__init__.py
+++ stado/plugins/__init__.py
@@ -1,8 +1,8 @@
 class Plugin:
     pass
 
-class Extension(Plugin):
 
+class Extension(Plugin):
     name = None
     extensions = []
 
@@ -13,22 +13,21 @@ class Extension(Plugin):
 
 
     def __init__(self, site):
+
+        # Reference to parent Site object.
         self.site = site
 
         self.renderers = self._update_template_engine(self.renderers)
 
+        # Model with supporting any extension.
         if self.extensions is None:
             self.site.item_types.set(None, self.loaders, self.renderers,
-                                        self.deployer)
+                                     self.deployer)
         else:
             for e in self.extensions:
-
-                self.site.item_types.set(
-                    e,
-                    loaders=self.loaders,
-                    renderers=self.renderers,
-                    deployers=self.deployer
-                )
+                self.site.item_types.set(e, loaders=self.loaders,
+                                         renderers=self.renderers,
+                                         deployers=self.deployer)
 
 
     def _update_template_engine(self, renderers):
@@ -39,7 +38,6 @@ class Extension(Plugin):
                 else x for x in renderers]
 
 
-
 # Supported files types.
 
 from .extensions.default import Default
@@ -47,6 +45,7 @@ from .extensions.markdown import Markdown
 from .extensions.html import HTML
 from .extensions.json import Json
 from .extensions.yaml import Yaml
+
 
 def load(select=None):
     """Yields controllers modules.
