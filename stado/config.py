1f6673b34fc6bc141a81f2fb028e53e8efe7266e
lecnim <kasper.minciel@gmail.com>
2013-10-30 01:23:51 +0100

    Removed unneeded variables in config.

--- stado/config.py
+++ stado/config.py
@@ -16,11 +16,6 @@ watch_interval = 1
 def get_default_site_config():
     """Returns dict with default site configuration."""
     return {
-        # Cache system: shelve or dict.
-        'cache': 'shelve',
-
-        # List of loaders names. If None, all loader are loaded.
-        'loaders': None,
         # List of controllers names. If None, all controllers are loaded.
         'controllers': None,
         'plugins': None,
