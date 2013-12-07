ab0cd1dada36935ef516a6fe674216fce74e3cd9
lecnim <kasper.minciel@gmail.com>
2013-12-07 21:08:21 +0100

    Tests running do not require nose.

--- runtests.py
+++ runtests.py
@@ -1,23 +1,44 @@
-import nose
 import sys
-import subprocess
+import runpy
+import unittest
+
+from stado import config
 
 
-# Testing source.
-print('Testing stado source...\n')
-if not nose.run():
-    sys.exit(1)
+if __name__ == "__main__":
+
+    config.log_level = 'CRITICAL'
+
+    # Building stado package.
+    runpy.run_module('build', run_name='__main__')
+    sys.path.insert(0, 'build/stado.py')
+
+    # Testing source.
+
+    print('\nTesting stado package...')
+
+    pattern = "test*.py"
+
+    suite = unittest.TestLoader().discover('tests/core', pattern=pattern)
+    suite.addTest(unittest.TestLoader().discover('tests/controllers', pattern=pattern))
+    suite.addTest(unittest.TestLoader().discover('tests/examples', pattern=pattern))
+    suite.addTest(unittest.TestLoader().discover('tests/templates', pattern=pattern))
+
+    # Skipping console tests using -q or quick argument.
+    if '-q' in sys.argv or 'quick' in sys.argv:
+        print('Quick testing, skipping console tests (development server, watcher)')
+    else:
+        print('Now some weird things will be printed, do not bother:')
+        suite.addTest(unittest.TestLoader().discover('tests/console', pattern=pattern))
+
+    result = unittest.TextTestRunner(verbosity=1).run(suite)
 
 
-# Compiling.
-print('')
-subprocess.call('python compile.py', shell=True)
+    if result.failures or result.errors:
+        # Some tests failed!
+        print('\nOops! Something went wrong! :(')
+        sys.exit(1)
 
-# Testing compiled package.
-print('Testing stado compiled package...\n')
-sys.path.insert(0, 'build/stado.py')
-if not nose.run():
-    sys.exit(1)
-
-# Everything ok.
-sys.exit(0)
+    # Everything ok.
+    print('\nDone! Everything OK!')
+    sys.exit(0)
