432560af2a305d4bc59651eae740de024f835e8e
lecnim <kasper.minciel@gmail.com>
2014-03-21 01:16:34 +0100

    Fixed missing calling in Markdown plugin test.

--- tests/plugins/markdown/test.py
+++ tests/plugins/markdown/test.py
@@ -8,7 +8,7 @@ class TestMarkdown(TestPlugin):
     """
     def setUp(self):
         super().setUp()
-        self.plugin = plugins.load_plugin('markdown')
+        self.plugin = plugins.load_plugin('markdown')()
 
     # plugin module
 
@@ -16,6 +16,6 @@ class TestMarkdown(TestPlugin):
         """should correctly convert item markdown source to html"""
 
         item = self.site.load('page.md')
-        self.plugin(item)
+        self.plugin.apply(item)
 
         self.assertEqual('<h1>header</h1>', item.source)
