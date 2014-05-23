efb2c1be51a4159ef625689b8fc66c15e6f25741
lecnim <kasper.minciel@gmail.com>
2014-05-23 23:48:27 +0200

    Updated utils tests.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -10,9 +10,11 @@ class TestRelativePath(unittest.TestCase):
     def test(self):
         """should correctly convert paths to elegant relative form"""
 
-        # TODO:
-        # self.assertEqual('a', utils.relative_path('a/'))
-        # self.assertEqual('a', utils.relative_path('\\a\\'))
-
+        self.assertEqual('a', utils.relative_path('a/'))
         self.assertEqual(path.join('a', 'b'), utils.relative_path('a\\b'))
         self.assertEqual(path.join('a', 'b'), utils.relative_path('a//b'))
+
+    def test_error(self):
+        """should raise error if path is absolute"""
+
+        self.assertRaises(ValueError, utils.relative_path, '/a')
