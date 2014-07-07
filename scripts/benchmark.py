8af825bf29cd5e99149e887e28037e54f665a6f4
lecnim <kasper.minciel@gmail.com>
2014-07-07 15:21:49 +0200

    Change benchmark.py

--- scripts/benchmark.py
+++ scripts/benchmark.py
@@ -4,15 +4,14 @@ import time
 import os
 
 from stado import log
-from stado import Stado
-# from stado.core.cache import ShelveCache
+from stado import Site
 
 
 log.setLevel('INFO')
 temp_path = tempfile.mkdtemp()
 
 
-def test_cache(number=50, path='tests/examples/markdown'):
+def benchmark(number=50, path='tests/examples/markdown'):
 
     files = len(os.listdir(path))
 
@@ -20,20 +19,11 @@ def test_cache(number=50, path='tests/examples/markdown'):
           '{} files per site = {} files.'.format(number, files, number*files))
 
     t = time.clock()
-    app = Stado(path, output=temp_path)
+    app = Site(path, output=temp_path)
     for i in range(number):
         app.build()
 
     print('{} s.'.format(round(time.clock() - t, 3)))
-    #
-    # t = time.clock()
-    # app = Stado(path, output=temp_path, cache=ShelveCache)
-    # for i in range(number):
-    #     app.run()
-    #
-    # print('- ShelveCache: \t{} s.'.format(round(time.clock() - t, 3)))
 
-
-test_cache()
-
+benchmark()
 shutil.rmtree(temp_path)
